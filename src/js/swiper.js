03ab613a3d513ea69a4e751a187c7d576d537c5a
Kyrylko <kkuzmenko1992@gmail.com>
2025-05-04 23:03:22 -0230

    completed section products, added swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,54 @@
+import Swiper from "swiper";
+import "swiper/css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  // initial swiper
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 40,
+
+    on: {
+      afterInit(sw) {
+        updateButtons(sw);
+      },
+      slideChange(sw) {
+        updateButtons(sw);
+      },
+    },
+  });
+
+  function updateButtons(sw) {
+    const isPrevDisabled = sw.isBeginning;
+    const isNextDisabled = sw.isEnd;
+
+    // left button
+    prevBtn.disabled = isPrevDisabled;
+    prevBtn.querySelector(".default-arrow").style.display = isPrevDisabled
+      ? "block" 
+      : "none";
+    prevBtn.querySelector(".disabled-arrow").style.display = isPrevDisabled
+      ? "none"
+      : "block"; 
+
+    // right button
+    nextBtn.disabled = isNextDisabled;
+    nextBtn.querySelector(".default-arrow").style.display = isNextDisabled
+      ? "block" 
+      : "none";
+    nextBtn.querySelector(".disabled-arrow").style.display = isNextDisabled
+      ? "none"
+      : "block"; 
+  }
+
+  prevBtn.addEventListener("click", () => {
+    swiper.slidePrev();
+  });
+
+  nextBtn.addEventListener("click", () => {
+    swiper.slideNext();
+  });
+});
